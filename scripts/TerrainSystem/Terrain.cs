6cba700f4b434a7346797048302bbf64764bcb7c
Jeremy <ggajy123@gmail.com>
2025-09-28 12:50:56 -0700

    terrain shader + git lfs

--- scripts/TerrainSystem/Terrain.cs
+++ scripts/TerrainSystem/Terrain.cs
@@ -1,168 +1,179 @@
 using Godot;
-using Godot.Collections; // Important: Add this using statement
+using Godot.Collections;
 
 [GlobalClass]
 public partial class Terrain : Node3D
 {
-	[ExportGroup("Dimensions")]
-	[Export] public int TerrainWidth = 100; // World units
-	[Export] public int TerrainDepth = 100; // World units
-	[Export(PropertyHint.Range, "2, 512")] public int Resolution = 128; // Number of vertices on one side
+    [ExportGroup("Dimensions")]
+    [Export] public int TerrainWidth = 100;
+    [Export] public int TerrainDepth = 100;
+    [Export(PropertyHint.Range, "2, 512")] public int Resolution = 128;
 
-	[ExportGroup("Generation")]
-	// This is the line that has been changed back
-	[Export] public TerrainLayer[] Layers { get; set; } = [];
+    [ExportGroup("Generation")]
+    [Export] public TerrainLayer[] Layers { get; set; } = [];
 
-	// This bool acts as a button in the inspector to regenerate the terrain
-	private bool _generate;
-	[Export]
-	public bool Generate
-	{
-		get => _generate;
-		set
-		{
-			_generate = value;
-			if (_generate)
-			{
-				GenerateTerrain();
-			}
-		}
-	}
+    [ExportGroup("Materials")]
+    [Export] public ShaderMaterial SplatmapMaterial { get; set; }
+    [Export] public TerrainMaterial[] Materials { get; set; } = new TerrainMaterial[MAX_TEXTURES];
 
-	private MeshInstance3D _meshInstance;
-	private const int MAX_TEXTURES = 4; // Corresponds to RGBA channels in vertex color
+    private bool _generate;
+    [Export]
+    public bool Generate
+    {
+        get => _generate;
+        set
+        {
+            _generate = value;
+            if (_generate)
+            {
+                GenerateTerrain();
+            }
+        }
+    }
 
-	public void GenerateTerrain()
-	{
-		// Changed Layers.Count to Layers.Length
-		if (Layers == null || Layers.Length == 0)
-		{
-			GD.PrintErr("No terrain layers assigned. Cannot generate terrain.");
-			return;
-		}
+    private MeshInstance3D _meshInstance;
+    private const int MAX_TEXTURES = 4;
 
-		// ... (rest of the file is unchanged)
-		// Ensure MeshInstance3D child exists
-		_meshInstance = GetNodeOrNull<MeshInstance3D>("TerrainMesh");
-		if (_meshInstance == null)
-		{
-			_meshInstance = new MeshInstance3D();
-			_meshInstance.Name = "TerrainMesh";
-			AddChild(_meshInstance);
-		}
+    public void GenerateTerrain()
+    {
+        if (Layers == null || Layers.Length == 0)
+        {
+            GD.PrintErr("No terrain layers assigned. Cannot generate terrain.");
+            return;
+        }
 
-		// 1. Process Layers
-		var terrainData = new TerrainData(Resolution, MAX_TEXTURES);
-		foreach (var layer in Layers)
-		{
-			if (layer != null)
-			{
-				layer.Apply(terrainData, Resolution);
-			}
-		}
+        _meshInstance = GetNodeOrNull<MeshInstance3D>("TerrainMesh");
+        if (_meshInstance == null)
+        {
+            _meshInstance = new MeshInstance3D();
+            _meshInstance.Name = "TerrainMesh";
+            AddChild(_meshInstance);
+        }
 
-		// 2. Normalize Splatmap Data
-		NormalizeSplatmap(terrainData);
+        var terrainData = new TerrainData(Resolution, MAX_TEXTURES);
+        foreach (var layer in Layers)
+        {
+            if (layer != null)
+            {
+                layer.Apply(terrainData, Resolution);
+            }
+        }
 
-		// 3. Build Mesh from final data
-		BuildMesh(terrainData);
+        NormalizeSplatmap(terrainData);
+        BuildMesh(terrainData);
 
-		GD.Print("Terrain generated successfully.");
-	}
+        GD.Print("Terrain generated successfully.");
+    }
 
-	private void NormalizeSplatmap(TerrainData data)
-	{
-		for (int z = 0; z < Resolution; z++)
-		{
-			for (int x = 0; x < Resolution; x++)
-			{
-				float totalStrength = 0;
-				for (int i = 0; i < MAX_TEXTURES; i++)
-				{
-					totalStrength += data.Splatmap[x, z, i];
-				}
+    private void NormalizeSplatmap(TerrainData data)
+    {
+        for (int z = 0; z < Resolution; z++)
+        {
+            for (int x = 0; x < Resolution; x++)
+            {
+                float totalStrength = 0;
+                for (int i = 0; i < MAX_TEXTURES; i++)
+                {
+                    totalStrength += data.Splatmap[x, z, i];
+                }
 
-				if (totalStrength > 0)
-				{
-					for (int i = 0; i < MAX_TEXTURES; i++)
-					{
-						data.Splatmap[x, z, i] /= totalStrength;
-					}
-				}
-				else
-				{
-					// If no texture is applied, default to the first one
-					data.Splatmap[x, z, 0] = 1.0f;
-				}
-			}
-		}
-	}
+                if (totalStrength > 0)
+                {
+                    for (int i = 0; i < MAX_TEXTURES; i++)
+                    {
+                        data.Splatmap[x, z, i] /= totalStrength;
+                    }
+                }
+                else
+                {
+                    data.Splatmap[x, z, 0] = 1.0f;
+                }
+            }
+        }
+    }
 
-	private void BuildMesh(TerrainData data)
-	{
-		var st = new SurfaceTool();
-		st.Begin(Mesh.PrimitiveType.Triangles);
+    private void BuildMesh(TerrainData data)
+    {
+        var st = new SurfaceTool();
+        st.Begin(Mesh.PrimitiveType.Triangles);
 
-		// Define vertex attributes for the splatmap shader
-		st.SetColor(new Color(1, 1, 1));
-		st.SetUV(new Vector2(0, 0));
+        st.SetColor(new Color(1, 1, 1));
+        st.SetUV(new Vector2(0, 0));
 
-		// Create Vertices
-		for (int z = 0; z < Resolution; z++)
-		{
-			for (int x = 0; x < Resolution; x++)
-			{
-				float xPos = (float)x / (Resolution - 1) * TerrainWidth;
-				float zPos = (float)z / (Resolution - 1) * TerrainDepth;
-				float yPos = data.Heights[x, z];
+        for (int z = 0; z < Resolution; z++)
+        {
+            for (int x = 0; x < Resolution; x++)
+            {
+                float xPos = (float)x / (Resolution - 1) * TerrainWidth;
+                float zPos = (float)z / (Resolution - 1) * TerrainDepth;
+                float yPos = data.Heights[x, z];
 
-				Vector2 uv = new Vector2((float)x / (Resolution - 1), (float)z / (Resolution - 1));
-				Color splatColor = new Color(
-					data.Splatmap[x, z, 0],
-					data.Splatmap[x, z, 1],
-					data.Splatmap[x, z, 2],
-					data.Splatmap[x, z, 3]
-				);
+                Vector2 uv = new Vector2((float)x / (Resolution - 1), (float)z / (Resolution - 1));
+                Color splatColor = new Color(
+                    data.Splatmap[x, z, 0],
+                    data.Splatmap[x, z, 1],
+                    data.Splatmap[x, z, 2],
+                    data.Splatmap[x, z, 3]
+                );
 
-				st.SetUV(uv);
-				st.SetColor(splatColor); // Pack splat weights into vertex color
-				st.AddVertex(new Vector3(xPos, yPos, zPos));
-			}
-		}
+                st.SetUV(uv);
+                st.SetColor(splatColor);
+                st.AddVertex(new Vector3(xPos, yPos, zPos));
+            }
+        }
 
-		// Create Indices
-		for (int z = 0; z < Resolution - 1; z++)
-		{
-			for (int x = 0; x < Resolution - 1; x++)
-			{
-				int topLeft = z * Resolution + x;
-				int topRight = topLeft + 1;
-				int bottomLeft = (z + 1) * Resolution + x;
-				int bottomRight = bottomLeft + 1;
+        for (int z = 0; z < Resolution - 1; z++)
+        {
+            for (int x = 0; x < Resolution - 1; x++)
+            {
+                int topLeft = z * Resolution + x;
+                int topRight = topLeft + 1;
+                int bottomLeft = (z + 1) * Resolution + x;
+                int bottomRight = bottomLeft + 1;
 
-				// First triangle
-				st.AddIndex(topLeft);
-				st.AddIndex(bottomLeft);
-				st.AddIndex(topRight);
+                st.AddIndex(topLeft);
+                st.AddIndex(topRight);
+                st.AddIndex(bottomLeft);
 
-				// Second triangle
-				st.AddIndex(topRight);
-				st.AddIndex(bottomLeft);
-				st.AddIndex(bottomRight);
-			}
-		}
+                st.AddIndex(topRight);
+                st.AddIndex(bottomRight);
+                st.AddIndex(bottomLeft);
+            }
+        }
 
-		st.GenerateNormals();
-		st.GenerateTangents();
+        st.GenerateNormals();
+        st.GenerateTangents();
 
-		var arrayMesh = st.Commit();
-		_meshInstance.Mesh = arrayMesh;
+        var arrayMesh = st.Commit();
+        _meshInstance.Mesh = arrayMesh;
 
-		// Add a simple material for visualization until you have a splat shader
-		var defaultMaterial = new StandardMaterial3D
-		{
-			VertexColorUseAsAlbedo = true
-		};
-		_meshInstance.MaterialOverride = defaultMaterial;
-	}
+        _meshInstance.MaterialOverride = SplatmapMaterial;
+
+        if (SplatmapMaterial != null)
+        {
+            var albedoTextures = new Godot.Collections.Array<Texture2D>();
+            var normalTextures = new Godot.Collections.Array<Texture2D>();
+            var roughnessTextures = new Godot.Collections.Array<Texture2D>();
+
+            for (int i = 0; i < MAX_TEXTURES; i++)
+            {
+                if (Materials[i] != null)
+                {
+                    albedoTextures.Add(Materials[i].AlbedoTexture);
+                    normalTextures.Add(Materials[i].NormalTexture);
+                    roughnessTextures.Add(Materials[i].RoughnessTexture);
+                }
+                else
+                {
+                    albedoTextures.Add(null);
+                    normalTextures.Add(null);
+                    roughnessTextures.Add(null);
+                }
+            }
+
+            SplatmapMaterial.SetShaderParameter("albedo_textures", albedoTextures);
+            SplatmapMaterial.SetShaderParameter("normal_textures", normalTextures);
+            SplatmapMaterial.SetShaderParameter("roughness_textures", roughnessTextures);
+        }
+    }
 }
---
+++ scripts/TerrainSystem/TerrainMaterial.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+[GlobalClass]
+public partial class TerrainMaterial : Resource
+{
+    [Export] public Texture2D AlbedoTexture { get; set; }
+    [Export] public Texture2D NormalTexture { get; set; }
+    [Export] public Texture2D RoughnessTexture { get; set; }
+}
