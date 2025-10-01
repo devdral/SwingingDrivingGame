e15fb6390efd86de28af34952acc7cc17d101a73
WMsans <141427510+WMsans@users.noreply.github.com>
2025-10-01 08:42:07 -0700

    add metalic layer

--- scripts/TerrainSystem/TerrainMaterial.cs
+++ scripts/TerrainSystem/TerrainMaterial.cs
@@ -3,7 +3,8 @@ using Godot;
 [GlobalClass]
 public partial class TerrainMaterial : Resource
 {
-    [Export] public Texture2D AlbedoTexture { get; set; }
-    [Export] public Texture2D NormalTexture { get; set; }
-    [Export] public Texture2D RoughnessTexture { get; set; }
+	[Export] public Texture2D AlbedoTexture { get; set; }
+	[Export] public Texture2D NormalTexture { get; set; }
+	[Export] public Texture2D RoughnessTexture { get; set; }
+	[Export] public Texture2D MetallicTexture { get; set; }
 }
